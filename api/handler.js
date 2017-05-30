7851963ae140d61eb97213915b3c77ee1007ffa4
yamatatsu <the.kinnikuman@gmail.com>
2017-05-30 23:42:07 +0900

    どーん

--- api/handler.js
+++ api/handler.js
@@ -1,16 +1,68 @@
 'use strict';
+const { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, buildSchema } = require('graphql');
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
 
-  callback(null, response);
+const UserType = genUserType(GraphQLString)
+const QueryType = genQueryType(UserType)
+const Schema = genGraphQLSchema(QueryType)
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.graphql = (event, context, callback) => {
+  const { query, variables } = JSON.parse(event.body);
+
+  graphql(Schema, query, null, {}, variables)
+    .then((response) => callback(null, createResponse(200, response)))
+    .catch((error) => callback(null, createResponse(error.responseStatusCode || 500, { message: error.message || 'Internal server error' })));
 };
+
+////////////
+// private
+
+function createResponse(statusCode, body) {
+  return {
+    statusCode,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS
+    },
+    body: JSON.stringify(body),
+  }
+}
+
+function genUserType(_GraphQLString) {
+  return new GraphQLObjectType({
+    name: 'User',
+    description: 'A person who uses our app',
+    fields: () => ({
+      id: {
+        type: _GraphQLString,
+        description: 'The unique Identifier of the user',
+      },
+      name: {
+        type: _GraphQLString,
+        description: 'The name of the user',
+      },
+    }),
+  })
+}
+function genQueryType(_UserType) {
+  return new GraphQLObjectType({
+    name: 'Query',
+    fields: () => ({
+      // Add your own root fields here
+      viewer: {
+        type: _UserType,
+        resolve: (_, _args, context) => (
+          {
+            _type: 'user',
+            id: '1',
+            name: 'スヌーピー',
+          }
+        ),
+      },
+    }),
+  })
+}
+function genGraphQLSchema(_QueryType) {
+  return new GraphQLSchema({
+    query: _QueryType,
+  });
+}
